079ae10aad3db57a1b5b7e8d8d29722b88989a5d
ewillink <ewillink>
2012-05-18 19:01:08 +0000

    First attempt at MMT project pages

--- downloads/index.php
+++ downloads/index.php
@@ -1,5 +1,5 @@
 <?php
-require_once ("../../includes/buildServer-common.php");
+require_once ("/modeling/includes/buildServer-common.php");
 require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php"); require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php");  require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); $App = new App(); $Nav = new Nav(); $Menu = new Menu(); include($App->getProjectCommon());
 
 /* temporary redirect for emft projects, except on build servers if downloads folder exists */
