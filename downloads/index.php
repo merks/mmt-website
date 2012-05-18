face3fd97a898cb82c18238fc2edfb6e4e383cdb
ewillink <ewillink>
2012-05-18 19:21:34 +0000

    First attempt at MMT project pages

--- downloads/index.php
+++ downloads/index.php
@@ -21,7 +21,7 @@ $dls = array(
 			"Automated Tests" => "automated-tests"
 		)
 	),*/
-	"/qvtd" => array(
+/*	"/qvtd" => array(
 		"QVT Declarative" => array(
 			"<acronym title=\"Archived Update Site\"><img alt=\"Click to download archived All-In-One p2 Repo Update Site\" src=\"/modeling/images/dl-icon-update-zip.gif\"/> <b style=\"color:green\">All-In-One Update Site</b></acronym>" => "incubation-Update",
 			"SDK (Runtime, Source, Examples)" => "incubation-SDK",
@@ -47,7 +47,7 @@ $dls = array(
 			"Examples" => "examples",
 			"Automated Tests" => "automated-tests"
 		)
-	)
+	) */
 );
 
 /* list of valid file prefixes for projects who have been renamed; keys have leading / to match $proj */
@@ -59,7 +59,7 @@ $filePre = array(
 /* define showNotes(), $oldrels, doLanguagePacks() in extras-$proj.php (or just extras.php for flat projects) if necessary, downloads-common.php will include them */
 /* end config */
 
-if ($isBuildServer) { include_once $_SERVER["DOCUMENT_ROOT"] . "/mmt/build/sideitems-common.php"; }
+/*if ($isBuildServer) { include_once $_SERVER["DOCUMENT_ROOT"] . "/mmt/build/sideitems-common.php"; } */
 require_once($_SERVER["DOCUMENT_ROOT"] . "/modeling/includes/downloads-common.php");
 
 $html = ob_get_contents();
@@ -68,12 +68,12 @@ ob_end_clean();
 $trans = array_flip($projects);
 $pageTitle = "Eclipse Modeling - MMT - $trans[$projct] - Downloads";
 $pageKeywords = ""; // TODO: add something here
-$pageAuthor = "";
+$pageAuthor = "Ed Willink";
 
 # Generate the web page
 $App->AddExtraHtmlHeader('<link rel="stylesheet" type="text/css" href="/modeling/includes/downloads.css"/>' . "\n");
 $App->AddExtraHtmlHeader('<script src="/modeling/includes/downloads.js" type="text/javascript"></script>' . "\n"); //ie doesn't understand self closing script tags, and won't even try to render the page if you use one
-$App->AddExtraHtmlHeader('<link type="application/rss+xml" rel="alternate" title="MMT '.$trans[$projct].' Build Feed" href="http://www.eclipse.org/modeling/download.php?file=/'.$PR.'/feeds/builds-'.$projct.'.xml"/>' . "\n");
+/*$App->AddExtraHtmlHeader('<link type="application/rss+xml" rel="alternate" title="MMT '.$trans[$projct].' Build Feed" href="http://www.eclipse.org/modeling/download.php?file=/'.$PR.'/feeds/builds-'.$projct.'.xml"/>' . "\n"); */
 $App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
 
 ?>
