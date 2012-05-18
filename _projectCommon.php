f43fd88fe2df3e47b4c428042c24f87b55952b84
ewillink <ewillink>
2012-05-18 17:35:28 +0000

    First attempt at MMT project pages

--- _projectCommon.php
+++ _projectCommon.php
@@ -1,124 +1,43 @@
 <?php
-$PR = "mmt";
-$projectName = "MMT";
-$defaultProj = "/atl";
 
-$isBuildServer = (preg_match("/^(emft|modeling|build)\.eclipse\.org$/", $_SERVER["SERVER_NAME"]));
-$isBuildDotEclipseServer = $_SERVER["SERVER_NAME"] == "build.eclipse.org";
-$isWWWserver = (preg_match("/^(?:www.|)eclipse.org$/", $_SERVER["SERVER_NAME"]));
-$isEclipseCluster = (preg_match("/^(?:www.||download.|download1.|build.)eclipse.org$/", $_SERVER["SERVER_NAME"]));
-$debug = (isset ($_GET["debug"]) && preg_match("/^\d+$/", $_GET["debug"]) ? $_GET["debug"] : -1);
-$writableRoot = ($isBuildServer ? $_SERVER["DOCUMENT_ROOT"] . "/modeling/includes/" : "/home/data/httpd/writable/www.eclipse.org/");
-$writableBuildRoot = $isBuildDotEclipseServer ? "/opt/public/modeling" : "/home/www-data";
+	# set default theme
+	$_theme = "Nova";
+	$theme = "";
+	if(isset($_POST['theme'])) {
+		$_theme = $_POST['theme'];
+	}
+	if($_theme != "" && $App->isValidTheme($_theme)) {
+		setcookie("theme", $_theme);
+		$theme = $_theme;
+	}
+	else {
+		# Get theme from browser, or none default
+		$theme = $App->getUserPreferedTheme();
+	}
 
-$rooturl = "http://" . $_SERVER["HTTP_HOST"] . "/$PR";
-$downurl = ($isBuildServer ? "" : "http://www.eclipse.org");
-$bugurl = "https://bugs.eclipse.org";
+	$Nav->setLinkList(array());
+	$branding = <<<EOBRANDING
+	<STYLE TYPE="text/css">
+	  .sideitem { border-width: 1px 1px; }
+	  body { font-size: small; }
+	  #midcolumn { margin-top: 5px; }
+	</STYLE>
 
-if (isset ($_GET["skin"]) && preg_match("/^(Blue|EclipseStandard|Industrial|Lazarus|Miasma|Modern|OldStyle|Phoenix|PhoenixTest|PlainText|Nova)$/", $_GET["skin"], $regs))
-{
-	$theme = $regs[1];
-}
-else
-{
-	$theme = "Nova";
-}
+EOBRANDING;
+	$Menu->setProjectBranding($branding);
 
-/* projects/components in cvs */
-/* "proj" => "cvsname" */
-$cvsprojs = array (); /* should always be empty */
-
-/* sub-projects/components in cvs for projects/components above (if any) */
-/* "cvsname" => array("shortname" => "cvsname") */
-$cvscoms = array(
-	"org.eclipse.m2m" => array (
-		"atl" => "org.eclipse.atl",
-		"qvtoml" => "org.eclipse.qvtoml",
-		"qvtd" => "org.eclipse.qvt.declarative",
-		/* add more here */
-	)
-);
-
-$projects = array(
-	"ATL" => "atl",
-	"Operational QVT" => "qvtoml",
-	"QVT Declarative" => "qvtd"
-);
-
-$bugcoms = array_flip($projects);
-$bugcoms = preg_replace("/ /", "%20", $bugcoms);
-
-$extraprojects = array(); //components with only downloads, no info yet, "prettyname" => "directory"
-$nodownloads = array(); //components with only information, no downloads, or no builds available yet, "projectkey"
-$nonewsgroup = array("atl"); //components without newsgroup
-$nomailinglist = array("atl"); //components without mailinglist
-$incubating = array("qvtd"); // components that are incubating
-$nomenclature = "Component"; //are we dealing with "components" or "projects"?
-
-include_once $_SERVER["DOCUMENT_ROOT"] . "/modeling/includes/scripts.php";
-
-$regs = null;
-$proj = (isset($_GET["project"]) && preg_match("/^(" . join("|", $projects) . ")$/", $_GET["project"], $regs) ? $regs[1] : getProjectFromPath($PR));
-$projct= preg_replace("#^/#", "", $proj);
-
-$buildtypes = array(
-	"R" => "Release",
-	"S" => "Stable",
-	"I" => "Integration",
-	"M" => "Maintenance",
-	"N" => "Nightly"
-);
-
-$Nav->setLinkList(array());
-$Nav->addCustomNav("About This Project", "/projects/project_summary.php?projectid=" . str_replace("/", ".", $PR), "", 1);
-$Nav->addNavSeparator($projectName, "$rooturl/");
-
-# after moving to modeling index.php format, use this
-/*foreach (array_keys(array_diff($projects, $extraprojects)) as $z)
-{
-	$Nav->addCustomNav($z, "$rooturl/?project=$projects[$z]", "_self", 2);
-}*/
-
-# and remove this
-foreach (array_keys(array_diff($projects, $extraprojects)) as $z)
-{
-	$Nav->addCustomNav($z, "http://wiki.eclipse.org/M2M/" . strtoupper($projects[$z]), "_self", 2);
-}
-
-$Nav->addNavSeparator("Downloads", "$downurl/$PR/downloads/?project=$proj");
-if ($projct == "atl") {
-$Nav->addCustomNav("Update Manager", "$rooturl/atl/updates/", "_self", 2);
-//$Nav->addCustomNav("Old Update Manager (ATL < 3.2)", "$rooturl/updates/", "_self", 2);
-} else {
-$Nav->addCustomNav("Update Manager", "$rooturl/updates/", "_self", 2);
-}
-$Nav->addNavSeparator("Documentation", "http://wiki.eclipse.org/Model_to_Model_Transformation_(MMT)");
-$Nav->addCustomNav("Plan", "http://www.eclipse.org/projects/project-plan.php?projectid=modeling.mmt", "_self", 2);
-#$Nav->addCustomNav("Plan", "$rooturl/docs/plans/mmt_project_plan_1_1.html", "_self", 2);
-$Nav->addCustomNav("Release Notes", "http://www.eclipse.org/$PR/news/relnotes.php?project=$proj&amp;version=HEAD", "_self", 2);
-$Nav->addCustomNav("Search CVS", "http://www.eclipse.org/$PR/searchcvs.php?q=file%3A+org.eclipse." . strtolower($projectName) . "%2F" . ($proj?"org.eclipse.".$proj."%2F":"") . "+days%3A+7", "_self", 2);
-
-$Nav->addNavSeparator("Community", "http://wiki.eclipse.org/Modeling_Corner");
-$Nav->addCustomNav("Wiki", "http://wiki.eclipse.org/Model_to_Model_Transformation_(MMT)", "_self", 2);
-$Nav->addCustomNav("Newsgroup", "news://news.eclipse.org/eclipse.modeling.mmt", "_self", 2);
-$Nav->addCustomNav("Modeling Corner", "http://wiki.eclipse.org/Modeling_Corner", "_self", 2);
-$collist = "&columnlist=changeddate%2Cbug_severity%2Cpriority%2Crep_platform%2Cbug_status%2Cproduct%2Ccomponent%2Cversion%2Ctarget_milestone%2short_desc";
-$Nav->addCustomNav("Open Bugs", "$bugurl/bugs/buglist.cgi?bug_status=NEW&bug_status=ASSIGNED&bug_status=REOPENED&product=" . $projectName . (false && isset ($bugcoms[$proj]) ? "&component=$bugcoms[$proj]" : "") . "&query_format=advanced&order=bug_status%2Ctarget_milestone%2Cbug_id$collist", "_self", 2);
-$Nav->addCustomNav("Submit A Bug", "$bugurl/bugs/enter_bug.cgi?product=" . $projectName, "_self", 2);
-$Nav->addCustomNav("Contributors", "http://www.eclipse.org/$PR/project-info/team.php", "_self", 2);
-
-//if ($projct == "atl")
-//{	
-//	$Nav->addNavSeparator("ATL", "/atl");
-//	$Nav->addCustomNav("Download", "http://www.eclipse.org/modeling/m2m/downloads/?project=atl", "_self", 3);
-//	$Nav->addCustomNav("Old Downloads", "/m2m/atl/download/index-old.php", "_self", 2);
-//	$Nav->addCustomNav("Documentation", "/atl/documentation", "_self", 3);
-//	$Nav->addCustomNav("Support", "/atl/support", "_self", 3);
-//	$Nav->addCustomNav("Getting Involved", "/atl/developers", "_self", 3);
-//}
-unset ($bugcoms);
-
-$App->AddExtraHtmlHeader("<link rel=\"stylesheet\" type=\"text/css\" href=\"/modeling/includes/common.css\"/>\n");
-addGoogleAnalyticsTrackingCodeToHeader();
-$App->Promotion = TRUE; # set true to enable current eclipse.org site-wide promo
+	# Define your project-wide Nav bars here.
+	# Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank), level (1, 2 or 3)
+	# these are optional
+	$Nav->addNavSeparator("Modeling", "/modeling/");
+	$Nav->addNavSeparator("M2M", "/m2m/");
+#	$Nav->addCustomNav("Download", "/m2m/download/", "_self", 1);
+#	$Nav->addCustomNav("Documentation", "/m2m/doc/", "_self", 1);
+#	$Nav->addCustomNav("Wiki", "http://wiki.eclipse.org/index.php/M2M", "_self", 1);
+	$Nav->addCustomNav("About This Project", "/projects/project_summary.php?projectid=modeling.m2m", "_self", 1);
+	$Nav->addCustomNav("Components", "/m2m/", "_self", 1);
+	$Nav->addCustomNav("Infrastructure", "/m2m/", "_self", 2);
+	$Nav->addCustomNav("ATL", "/modeling/m2m/atl/", "_self", 2);
+	$Nav->addCustomNav("Operational QVT", "/m2m/", "_self", 2);
+	$Nav->addCustomNav("Declarative QVT", "/m2m/", "_self", 2);
 ?>
---
+++ downloads/index.php
@@ -0,0 +1,85 @@
+<?php
+require_once ("../../includes/buildServer-common.php");
+require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php"); require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php");  require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); $App = new App(); $Nav = new Nav(); $Menu = new Menu(); include($App->getProjectCommon());
+
+/* temporary redirect for emft projects, except on build servers if downloads folder exists */
+if (isset($_GET["project"]) && isset($emft_redirects) && is_array($emft_redirects) && in_array($_GET["project"],$emft_redirects))
+{
+	header("Location: http://www.eclipse.org/emft/downloads/?project=" . $_GET["project"]);
+	exit;
+}
+ob_start();
+
+/* config */
+
+/* zips that are allowed to be absent from the downloads page (eg., new ones added mid-stream) */
+$extraZips = array();
+
+/* $project => sections/Project Name => (prettyname => filename) */
+/* only required if using something other than the default 4; otherwise will be generated */
+$dls = array(
+	/*"/newProj" => array(
+		"Project Name" => array( # same as value in _projectCommon.php's $projects array
+			"<acronym title=\"Click to download archived All-In-One p2 Repo Update Site\"><img alt=\"Click to download archived All-In-One p2 Repo Update Site\" src=\"/modeling/images/dl-icon-update-zip.gif\"/> <b style=\"color:green\">All-In-One Update Site</b></acronym>" => "Update",
+			"SDK (Runtime, Source)" => "SDK",
+			"Runtime" => "runtime",
+			"Examples" => "examples",
+			"Automated Tests" => "automated-tests"
+		)
+	),*/
+	"/qvtd" => array(
+		"QVT Declarative" => array(
+			"<acronym title=\"Archived Update Site\"><img alt=\"Click to download archived All-In-One p2 Repo Update Site\" src=\"/modeling/images/dl-icon-update-zip.gif\"/> <b style=\"color:green\">All-In-One Update Site</b></acronym>" => "incubation-Update",
+			"SDK (Runtime, Source, Examples)" => "incubation-SDK",
+			"Runtime" => "incubation-runtime",
+			"Examples" => "incubation-examples",
+			"Automated-Tests" => "incubation-Automated-Tests" # new Athena version
+		)
+	),
+	"/qvtoml" => array(
+		"QVTOML" => array(
+			"<acronym title=\"Archived Update Site\"><img alt=\"Click to download archived All-In-One p2 Repo Update Site\" src=\"/modeling/images/dl-icon-update-zip.gif\"/> <b style=\"color:green\">All-In-One Update Site</b></acronym>" => "Update",
+			"SDK (Runtime, Source, Examples)" => "SDK",
+			"Runtime" => "runtime",
+			"Examples" => "examples",
+			"Automated-Tests" => "Automated-Tests" # new Athena version
+		)
+	),
+	"/atl" => array(
+		"ATL" => array(
+			"<acronym title=\"Click to download archived All-In-One p2 Repo Update Site\"><img alt=\"Click to download archived All-In-One p2 Repo Update Site\" src=\"/modeling/images/dl-icon-update-zip.gif\"/> <b style=\"color:green\">All-In-One Update Site</b></acronym>" => "Update",
+			"SDK (Runtime, Source, Examples)" => "SDK",
+			"Runtime" => "runtime",
+			"Examples" => "examples",
+			"Automated Tests" => "automated-tests"
+		)
+	),
+);
+
+/* list of valid file prefixes for projects who have been renamed; keys have leading / to match $proj */
+/* only required if using something other than the default; otherwise will be generated */
+$filePre = array(
+	/* "/newproj" => array("m2m-newproj"), */
+);
+
+/* define showNotes(), $oldrels, doLanguagePacks() in extras-$proj.php (or just extras.php for flat projects) if necessary, downloads-common.php will include them */
+/* end config */
+
+if ($isBuildServer) { include_once $_SERVER["DOCUMENT_ROOT"] . "/modeling/m2m/build/sideitems-common.php"; }
+require_once($_SERVER["DOCUMENT_ROOT"] . "/modeling/includes/downloads-common.php");
+
+$html = ob_get_contents();
+ob_end_clean();
+
+$trans = array_flip($projects);
+$pageTitle = "Eclipse Modeling - M2M - $trans[$projct] - Downloads";
+$pageKeywords = ""; // TODO: add something here
+$pageAuthor = "";
+
+# Generate the web page
+$App->AddExtraHtmlHeader('<link rel="stylesheet" type="text/css" href="/modeling/includes/downloads.css"/>' . "\n");
+$App->AddExtraHtmlHeader('<script src="/modeling/includes/downloads.js" type="text/javascript"></script>' . "\n"); //ie doesn't understand self closing script tags, and won't even try to render the page if you use one
+$App->AddExtraHtmlHeader('<link type="application/rss+xml" rel="alternate" title="M2M '.$trans[$projct].' Build Feed" href="http://www.eclipse.org/modeling/download.php?file=/'.$PR.'/feeds/builds-'.$projct.'.xml"/>' . "\n");
+$App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
+
+?>
