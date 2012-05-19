c5bceadc2059c4505f133da7759b4bc8cf1041a7
ewillink <ewillink>
2012-05-19 13:46:45 +0000

    First attempt at MMT project pages

--- _projectCommon.php
+++ _projectCommon.php
@@ -59,7 +59,7 @@ $bugcoms = preg_replace("/ /", "%20", $bugcoms);
 
 $extraprojects = array(); //components with only downloads, no info yet, "prettyname" => "directory"
 $nodownloads = array(); //components with only information, no downloads, or no builds available yet, "projectkey"
-$nonewsgroup = array("atl","qvtd","qvto"); //components without newsgroup
+$nonewsgroup = array(); //components without newsgroup
 $nomailinglist = array("atl","qvtd","qvto"); //components without mailinglist
 $incubating = array("qvtd"); // components which are still incubating
 $nomenclature = "Component"; //are we dealing with "components" or "projects"?
@@ -86,9 +86,9 @@ foreach (array_keys(array_diff($projects, $extraprojects)) as $z)
 	$Nav->addCustomNav($z, "$rooturl/?project=$projects[$z]", "_self", 2);
 }
 
-$Nav->addNavSeparator("MMT Downloads", "/$PR/downloads/?project=$proj");
-$Nav->addNavSeparator("M2M Downloads", "/modeling/m2m/downloads/?project=$proj");
+$Nav->addNavSeparator("MMT Downloads", "$downurl/$PR/downloads/?project=$proj");
 $Nav->addCustomNav("Update Manager", "$rooturl/updates/", "_self", 2);
+$Nav->addNavSeparator("M2M Downloads", "$downurl/modeling/m2m/downloads/?project=$proj");
 
 $Nav->addNavSeparator("Documentation", "$rooturl/docs.php?project=$proj");
 $Nav->addCustomNav("FAQ", "$rooturl/faq.php?project=$proj", "_self", 2);
@@ -109,45 +109,4 @@ unset ($bugcoms);
 $App->AddExtraHtmlHeader("<link rel=\"stylesheet\" type=\"text/css\" href=\"/modeling/includes/common.css\"/>\n");
 addGoogleAnalyticsTrackingCodeToHeader();
 $App->Promotion = TRUE; # set true to enable current eclipse.org site-wide promo
-
-/*	# set default theme
-	$_theme = "Nova";
-	$theme = "";
-	if(isset($_POST['theme'])) {
-		$_theme = $_POST['theme'];
-	}
-	if($_theme != "" && $App->isValidTheme($_theme)) {
-		setcookie("theme", $_theme);
-		$theme = $_theme;
-	}
-	else {
-		# Get theme from browser, or none default
-		$theme = $App->getUserPreferedTheme();
-	}
-
-	$Nav->setLinkList(array());
-	$branding = <<<EOBRANDING
-	<STYLE TYPE="text/css">
-	  .sideitem { border-width: 1px 1px; }
-	  body { font-size: small; }
-	  #midcolumn { margin-top: 5px; }
-	</STYLE>
-
-EOBRANDING;
-	$Menu->setProjectBranding($branding);
-
-	# Define your project-wide Nav bars here.
-	# Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank), level (1, 2 or 3)
-	# these are optional
-	$Nav->addNavSeparator("Modeling", "/modeling/");
-	$Nav->addNavSeparator("M2M", "/m2m/");
-#	$Nav->addCustomNav("Download", "/m2m/download/", "_self", 1);
-#	$Nav->addCustomNav("Documentation", "/m2m/doc/", "_self", 1);
-#	$Nav->addCustomNav("Wiki", "http://wiki.eclipse.org/index.php/M2M", "_self", 1);
-	$Nav->addCustomNav("About This Project", "/projects/project_summary.php?projectid=modeling.m2m", "_self", 1);
-	$Nav->addCustomNav("Components", "/m2m/", "_self", 1);
-	$Nav->addCustomNav("Infrastructure", "/m2m/", "_self", 2);
-	$Nav->addCustomNav("ATL", "/modeling/m2m/atl/", "_self", 2);
-	$Nav->addCustomNav("Operational QVT", "/m2m/", "_self", 2);
-	$Nav->addCustomNav("Declarative QVT", "/m2m/", "_self", 2); */
 ?>
