78ea6bb741d211316247e17cb837b404c5fe0e06
ewillink <ewillink>
2012-06-07 09:50:51 +0000

    Support ATL archives

---
+++ downloads/extras-atl.php
@@ -0,0 +1,22 @@
+<?php
+
+$oldrels = array(
+	"3.2.0" => "201106080518",
+	"3.1.1" => "201009141132",
+	"3.1.0" => "201006150240"
+);
+
+function showNotes()
+{
+?>
+        <div class="homeitem3col">
+                <h3>Questions?</h3>
+                <p>If you have problems downloading the drops, contact the <a href="mailto:webmaster@eclipse.org">webmaster</a>.</p>
+                <p>These are the minimum required downloads for using ATL:</p>
+                <ul>
+                        <li>To use ATL, you require both the ATL &amp; <a href="/modeling/emf/downloads/?project=emf">EMF</a> Runtimes.</li>
+                </ul>
+                <p>All downloads are provided under the terms and conditions of the <a href="http://www.eclipse.org/legal/epl/notice.html">Eclipse Foundation Software User Agreement</a> unless otherwise specified.</p>
+        </div>
+<?php
+}
