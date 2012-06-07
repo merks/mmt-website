ec5eb61001843714152c97233da5b6368d3286cf
ewillink <ewillink>
2012-06-07 10:25:42 +0000

    Support QVTo archives

---
+++ downloads/extras-qvto.php
@@ -0,0 +1,24 @@
+<?php
+
+$oldrels = array(
+	"3.0.0" => "201006151231",
+	"2.0.1" => "200909011247",
+	"2.0.0" => "200906161204",
+	"1.0.1" => "200811081449",
+	"1.0.0" => "200806110744"
+);
+
+function showNotes()
+{
+?>
+        <div class="homeitem3col">
+                <h3>Questions?</h3>
+                <p>If you have problems downloading the drops, contact the <a href="mailto:webmaster@eclipse.org">webmaster</a>.</p>
+                <p>These are the minimum required downloads for using QVTo:</p>
+                <ul>
+                        <li>To use QVTo, you require both the QVTo &amp; <a href="/modeling/emf/downloads/?project=emf">EMF</a> Runtimes.</li>
+                </ul>
+                <p>All downloads are provided under the terms and conditions of the <a href="http://www.eclipse.org/legal/epl/notice.html">Eclipse Foundation Software User Agreement</a> unless otherwise specified.</p>
+        </div>
+<?php
+}
