730674488ae3045b8f048208f316bc5a3c4e6d37
Ed.Willink <ed@willink.me.uk>
2012-12-19 20:37:45 +0000

    [qvtd] Archive 0.8.0

---
+++ downloads/extras-qvtd.php
@@ -0,0 +1,20 @@
+<?php
+
+$oldrels = array(
+	"0.8.0" => "201007101005"
+);
+
+function showNotes()
+{
+?>
+        <div class="homeitem3col">
+                <h3>Questions?</h3>
+                <p>If you have problems downloading the drops, contact the <a href="mailto:webmaster@eclipse.org">webmaster</a>.</p>
+                <p>These are the minimum required downloads for using QVTd:</p>
+                <ul>
+                        <li>To use QVTd, you require both the QVTd &amp; <a href="/modeling/emf/downloads/?project=emf">EMF</a> Runtimes.</li>
+                </ul>
+                <p>All downloads are provided under the terms and conditions of the <a href="http://www.eclipse.org/legal/epl/notice.html">Eclipse Foundation Software User Agreement</a> unless otherwise specified.</p>
+        </div>
+<?php
+}
